8c41221bd3d5410c6e6196e7b17bb3136d5c3183
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-03-06 14:36:43 +0100

    Impostazione Milestone 0 , e costruzione input per ricerca film

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   },
 
 
-  created() {
+  created() { 
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=129c7c147dc0e52f5990dc27c7d71e42&query=ritorno + al + futuro').then(res => {
       console.dir(res.data.results);
       this.store.movies = res.data.results
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -10,7 +10,7 @@ export default {
         }
     }
 }
-</script>
+</script> 
 
 <template>
     <div class="container">
